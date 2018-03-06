3afbe2077d0faacc9b926fdd100d5688c1345803
Sanket Chintal <sanket@247software.in>
2018-03-06 12:17:59 +0530

    m-1

--- src/ApiConsumer/Consumer.php
+++ src/ApiConsumer/Consumer.php
@@ -1,13 +1,6 @@
 <?php
 namespace ApiConsumer;
 
-# Story #777 | Changes related to IMS-Task
-# Manually add syntax error 
-functo ()
-{
-    
-}
-
 /**
  * Simple class/wrapper to allows easy creation of a GET url string to be passed
  * via Curl. The class currently assumes a JSON return which will be parsed to an
